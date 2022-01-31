e6e2104c3790bdbfd148a55ccc31530835fa0993
Daniel Crookall <danielc3@students.cs.ubc.ca>
2022-01-31 05:02:36 -0800

    Copied all test files from C0 repo to C1

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -18,18 +18,18 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		return Promise.reject("Not implemented.");
+		return Promise.resolve([]);
 	}
 
 	public removeDataset(id: string): Promise<string> {
-		return Promise.reject("Not implemented.");
+		return Promise.resolve("");
 	}
 
 	public performQuery(query: unknown): Promise<InsightResult[]> {
-		return Promise.reject("Not implemented.");
+		return Promise.resolve([]);
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
-		return Promise.reject("Not implemented.");
+		return Promise.resolve([]);
 	}
 }
