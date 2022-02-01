2dbe3e6c37c0ba285819b0b0dd1dfb9c05f099f9
Daniel Crookall <danielc3@students.cs.ubc.ca>
2022-02-01 03:57:53 -0800

    Began implementation of addDataSet.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -6,6 +6,7 @@ import {
 	InsightResult,
 	NotFoundError
 } from "./IInsightFacade";
+import JSZip from "jszip";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -13,12 +14,36 @@ import {
  *
  */
 export default class InsightFacade implements IInsightFacade {
+	private addedIds: string[] = [];
+
 	constructor() {
-		console.log("InsightFacadeImpl::init()");
+		let a = 5;
+
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		return Promise.resolve([]);
+		return new Promise((resolve, reject) => {
+			if(!this.isValidID(id)) { // reject if invalid ID or same as previously added
+				reject(new InsightError());
+			} else {
+				this.addedIds.push(id);
+				resolve(this.addedIds);
+			}
+
+		});
+	}
+
+	public isValidID(id: string){
+		if (id.includes("_") || this.addedIds.includes(id)) {
+			return false;
+		}
+		return id.replace(/\s/g, "").length; // removes all whitespace in string
+		// then checks length. if 0, the string was all whitespace and we return false
+	}
+
+	public async processDataset(content: string) {
+		let zip = new JSZip();
+		await zip.loadAsync(content,{base64: true});
 	}
 
 	public removeDataset(id: string): Promise<string> {
