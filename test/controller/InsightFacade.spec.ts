9e4f34e1cf7c4697f9a97565cea9ce78ea5c7d19
Daniel Crookall <danielc3@students.cs.ubc.ca>
2022-02-03 16:47:50 -0800

    Fixed bug in addDataset and refactored as async. Implemented loadDataset.

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -268,7 +268,7 @@ describe("InsightFacade", function () {
 		folderTest<unknown, Promise<InsightResult[]>, Error>(
 			"Add Dynamic Test",
 			(input: unknown): Promise<InsightResult[]> => facade.performQuery(input),
-			"./test/resources/queries",
+			"./test/resources/queries/singleQuery",
 			{
 				errorValidator(error: any): error is Error {
 					return error === "InsightError" || error === "ResultTooLargeError";
