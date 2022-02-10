286a2a2cfa85c96ad752f8bd0de5f32bbb3d9db3
Daniel Crookall <danielc3@students.cs.ubc.ca>
2022-02-09 18:20:02 -0800

    Testing for container timeout

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -50,91 +50,91 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public async removeDataset(id: string): Promise<string> {
-		let dataProcessing = new DatasetProcessing();
-		let addedIds: string[] = [];
-		if(!dataProcessing.isValidID(id, addedIds)){
-			return Promise.reject(new InsightError()); // invalid id
-		}
-		await dataProcessing.getExistingDataSetIds(addedIds);
-
-		if (!addedIds.includes(id)) {
-			return Promise.reject(new NotFoundError()); // valid id, but it has not been added yet
-		}
-
-		try {
-			fs.unlinkSync(this.dataDir + id + ".json"); // can be assured file exists at this point
-
-		} catch(err: any){
-			console.error(err.message);
-			return Promise.reject(new InsightError());
-		}
+		// let dataProcessing = new DatasetProcessing();
+		// let addedIds: string[] = [];
+		// if(!dataProcessing.isValidID(id, addedIds)){
+		// 	return Promise.reject(new InsightError()); // invalid id
+		// }
+		// await dataProcessing.getExistingDataSetIds(addedIds);
+		//
+		// if (!addedIds.includes(id)) {
+		// 	return Promise.reject(new NotFoundError()); // valid id, but it has not been added yet
+		// }
+		//
+		// try {
+		// 	fs.unlinkSync(this.dataDir + id + ".json"); // can be assured file exists at this point
+		//
+		// } catch(err: any){
+		// 	console.error(err.message);
+		// 	return Promise.reject(new InsightError());
+		// }
 
 
 		return Promise.resolve(id);
 	}
 
 	public async performQuery(query: unknown): Promise<InsightResult[]> {
-		let performQuery = new PerformQueryFilters();
-		let dataProcessor = new DatasetProcessing();
-		let dataset: any[];
-		let queryObject = performQuery.getQueryObject(query);
-		let filter = queryObject.WHERE;
-		let options = queryObject.OPTIONS;
-		let columns = options.COLUMNS;
-		let orderKey = options.ORDER;
-		let parser: any;
-		try {
-			parser = new QueryValidator(queryObject);
-			parser.queryValidation(queryObject); // checking here same reason as below
-			parser.whereValidation(queryObject.WHERE); // checking for validation of where block here, because it should only be checked once, not on subsequent iterations for nest queried like AND
-			parser.optionsValidation(options);
-			parser.validateFilter(filter);
-			parser.validateColumns(columns);
-			parser.validateOrder(orderKey, columns);
-
-			dataset = dataProcessor.loadDataset(parser.datasetID);
-		} catch (err: any){
-			console.error(err.message);
-			return Promise.reject(InsightError);
-		}
-		let queryResults: any;
-		let columnResults: any;
-		let orderedResults: any;
-		if(parser.isEmpty(filter)) {// empty where clause, ie. no filter, return all entries in dataset
-			columnResults = performQuery.performColumns(options, dataset);
-			orderedResults = performQuery.performOrder(options, columnResults); // note this will modify the array in place meaning column results will also be ordered automatically.
-		} else {
-			queryResults = performQuery.performFilter(filter,dataset);
-			columnResults = performQuery.performColumns(options, queryResults);
-			orderedResults = performQuery.performOrder(options, columnResults); // note this will modify the array in place meaning column results will also be ordered automatically.
-		}
-
-		if(orderedResults.length > 5000){
-			console.error("The result is too big. Only queries with a maximum of 5000 results are supported.");
-			return Promise.reject(ResultTooLargeError);
-		}
-		// console.log(orderedResults);
-		// console.log(orderedResults.length);
-		return Promise.resolve(orderedResults);
+		// let performQuery = new PerformQueryFilters();
+		// let dataProcessor = new DatasetProcessing();
+		// let dataset: any[];
+		// let queryObject = performQuery.getQueryObject(query);
+		// let filter = queryObject.WHERE;
+		// let options = queryObject.OPTIONS;
+		// let columns = options.COLUMNS;
+		// let orderKey = options.ORDER;
+		// let parser: any;
+		// try {
+		// 	parser = new QueryValidator(queryObject);
+		// 	parser.queryValidation(queryObject); // checking here same reason as below
+		// 	parser.whereValidation(queryObject.WHERE); // checking for validation of where block here, because it should only be checked once, not on subsequent iterations for nest queried like AND
+		// 	parser.optionsValidation(options);
+		// 	parser.validateFilter(filter);
+		// 	parser.validateColumns(columns);
+		// 	parser.validateOrder(orderKey, columns);
+		//
+		// 	dataset = dataProcessor.loadDataset(parser.datasetID);
+		// } catch (err: any){
+		// 	console.error(err.message);
+		// 	return Promise.reject(InsightError);
+		// }
+		// let queryResults: any;
+		// let columnResults: any;
+		// let orderedResults: any;
+		// if(parser.isEmpty(filter)) {// empty where clause, ie. no filter, return all entries in dataset
+		// 	columnResults = performQuery.performColumns(options, dataset);
+		// 	orderedResults = performQuery.performOrder(options, columnResults); // note this will modify the array in place meaning column results will also be ordered automatically.
+		// } else {
+		// 	queryResults = performQuery.performFilter(filter,dataset);
+		// 	columnResults = performQuery.performColumns(options, queryResults);
+		// 	orderedResults = performQuery.performOrder(options, columnResults); // note this will modify the array in place meaning column results will also be ordered automatically.
+		// }
+		//
+		// if(orderedResults.length > 5000){
+		// 	console.error("The result is too big. Only queries with a maximum of 5000 results are supported.");
+		// 	return Promise.reject(ResultTooLargeError);
+		// }
+		// // console.log(orderedResults);
+		// // console.log(orderedResults.length);
+		return Promise.resolve([]);
 	}
 
 	public async listDatasets(): Promise<InsightDataset[]> {
 		let dataProcessor = new DatasetProcessing();
-		let currentlyAddedDatsets = [];
-		if (fs.existsSync(this.dataDir)) {
-			const dir = await fs.promises.opendir(this.dataDir);
-			for await (const file of dir) {
-				let filename = file.name;
-				let id = path.parse(filename).name;
-				const numRows = dataProcessor.loadDataset(id).length;
-				const datasetInfo = {
-					id: id,
-					kind: InsightDatasetKind.Courses,
-					numRows: numRows
-				};
-				currentlyAddedDatsets.push(datasetInfo);
-			}
-		}
+		let currentlyAddedDatsets: any = [];
+		// if (fs.existsSync(this.dataDir)) {
+		// 	const dir = await fs.promises.opendir(this.dataDir);
+		// 	for await (const file of dir) {
+		// 		let filename = file.name;
+		// 		let id = path.parse(filename).name;
+		// 		const numRows = dataProcessor.loadDataset(id).length;
+		// 		const datasetInfo = {
+		// 			id: id,
+		// 			kind: InsightDatasetKind.Courses,
+		// 			numRows: numRows
+		// 		};
+		// 		currentlyAddedDatsets.push(datasetInfo);
+		// 	}
+		// }
 		return Promise.resolve(currentlyAddedDatsets);
 	}
 
