ef671e1eb0d074f0b7f6cfeb4311763fd51a52d5
Daniel Crookall <danielc3@students.cs.ubc.ca>
2022-03-20 02:38:47 -0700

    Work on required endpoints

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -1,16 +1,22 @@
 import express, {Application, Request, Response} from "express";
 import * as http from "http";
 import cors from "cors";
+import InsightFacade from "../controller/InsightFacade";
+import * as fs from "fs-extra";
+import {InsightDatasetKind, InsightError, NotFoundError} from "../controller/IInsightFacade";
+import {getContentFromArchives} from "../../test/TestUtil";
 
 export default class Server {
 	private readonly port: number;
 	private express: Application;
 	private server: http.Server | undefined;
+	private facade: InsightFacade;
 
 	constructor(port: number) {
 		console.info(`Server::<init>( ${port} )`);
 		this.port = port;
 		this.express = express();
+		this.facade = new InsightFacade();
 
 		this.registerMiddleware();
 		this.registerRoutes();
@@ -84,7 +90,93 @@ export default class Server {
 		// http://localhost:4321/echo/hello
 		this.express.get("/echo/:msg", Server.echo);
 
+		this.express.put("/dataset/:id/:kind", async (req: any, res: any) => {
+			const {id} = req.params;
+			const {kind} = req.params;
+			const rawData = req.body;
+			const content = Buffer.from(rawData).toString("base64");
+
+			let addedIds;
+			try {
+				addedIds = await this.facade.addDataset(id, content, kind);
+			} catch (err) {
+				return res.status(400).json(
+					{
+						error: "Failed to add dataset"
+					}
+				);
+			}
+			return res.status(200).json(
+				{
+					result: addedIds
+				}
+			);
+		});
+
+		this.express.delete("/dataset/:id", this.deleteDataset);
+		this.express.post("/query", this.performQuery);
+		this.express.get("/datasets", this.listDataset);
+
 		// TODO: your other endpoints should go here
+	}
+
+	private async listDataset(req: Request, res: Response) {
+		let addedDatasets;
+		addedDatasets = await this.facade.listDatasets();
+
+		return res.status(200).json(
+			{
+				result: addedDatasets
+			});
+
+	}
+
+	private async performQuery(req: Request, res: Response) {
+		const query = JSON.parse(req.body);
+
+		let queryResults;
+		try {
+			queryResults = await this.facade.performQuery(query);
+		} catch (err) {
+			return res.status(400).json(
+				{
+					error: "Failed to perform query"
+				}
+			);
+		}
+		return res.status(200).json(
+			{
+				result: queryResults
+			});
+
+	}
+
+	private async deleteDataset(req: Request, res: Response) {
+		const {id} = req.params;
+		let removedID;
+		try {
+			removedID = await this.facade.removeDataset(id);
+		} catch (err) {
+			if (err === InsightError) {
+				return res.status(400).json(
+					{
+						error: "Failed to remove dataset (InsightError)."
+					}
+				);
+			}
+			if (err === NotFoundError) {
+				return res.status(404).json(
+					{
+						error: "Failed to remove dataset (NotFoundError)"
+					}
+				);
+			}
+
+		}
+		return res.status(200).json(
+			{
+				result: removedID
+			});
 
 	}
 
